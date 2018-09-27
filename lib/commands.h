a2e8054db92caa1912cf948499765afa8721f762
Evangeline Archer <eroxyivy@yahoo.com>
2018-09-27 00:16:23 -0400

    Create commands.h

---
+++ lib/commands.h
@@ -0,0 +1,38 @@
+/*
+ * COMMANDS ARE LISTED HERE
+ */
+
+#define CMD_UNLOCK      unlock_car_cmd
+#define CMD_LOCK        lock_car_cmd
+
+#define CMD_WIN_OPEN    open_windows_cmd
+#define CMD_WIN_CLOSE   close_windows_cmd
+#define CMD_WIN_LVL     window_set_lvl_cmd-
+
+#define CMD_START       start_car_cmd
+#define CMD_KILL        kill_car_cmd
+
+#define CMD_DOOR_OPEN   open_door_cmd-
+#define CMD_DOOR_CLOSE  close_door_cmd-
+#define CMD_TRUNK_OPEN  open_trunk_cmd
+#define CMD_TRUNK_CLOSE close_trunk_cmd
+
+#define CMD_HONK        honk_cmd-
+#define CMD_LIGHTS_ON   lights_on_cmd
+#define CMD_LIGHTS_OFF  lights_off_cmd
+#define CMD_LIGHTS_SET  set_lights_cmd-
+
+#define CMD_AC_ON       ac_on_cmd
+#define CMD_AC_OFF      ac_off_cmd
+#define CMD_AC_STR      ac_strength_set_cmd-
+#define CMD_AC_COOL     ac_cool_cmd
+#define CMD_AC_HEAT     ac_heat_cmd
+
+#define CMD_RADIO_ON    radio_on_cmd
+#define CMD_RADIO_OFF   radio_off_cmd
+#define CMD_RADIO_VOL   radio_set_vol_cmd-
+#define CMD_RADIO_SRC   radio_set_src_cmd-
+
+#define CMD_WIPE        wiper_wipe_cmd
+#define CMD_WIPER_SET   wiper_set_cmd-
+#define CMD_WIPER_SPRAY wiper_spray_cmd-
